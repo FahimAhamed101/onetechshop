482c65fffd9a3e7ad6e60207e59fdd900183cddb
fahim <fahimahamedweb@gmail.com>
2023-06-08 21:02:35 +0600

    deploy

--- now_build_staticfiles.sh
+++ now_build_staticfiles.sh
@@ -2,14 +2,14 @@
 
 # Install Python 3.6 since it is missing in the Now build environment
 yum install -y https://centos6.iuscommunity.org/ius-release.rpm
-yum install -y python36u
+yum install -y python39u
 
 # Install pip
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-python3.6 get-pip.py
+python3.9 get-pip.py
 
 # Install project requirements
 pip install -r requirements.txt
 
 # Build staticfiles
-python3.6 manage.py collectstatic
+python3.9 manage.py collectstatic
