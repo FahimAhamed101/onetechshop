097867148855b4c34c39172ed337b79d32a741a8
fahim <fahimahamedweb@gmail.com>
2023-06-08 21:05:27 +0600

    adding

--- webshop/settings.py
+++ webshop/settings.py
@@ -72,23 +72,23 @@ WSGI_APPLICATION = 'webshop.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-#DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
-#}
 DATABASES = {
-    'default': {
-        'ENGINE': "django.db.backends.postgresql_psycopg2",
-        'HOST': os.environ['HOST'],
-        'NAME': os.environ['NAME'],
-        'USER': os.environ['USER'],
-        'PASSWORD': os.environ['PASSWORD'],
-        'PORT': os.environ['PORT'],
-
-    }
+     'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+         'NAME': BASE_DIR / 'db.sqlite3',
+     }
 }
+# DATABASES = {
+    #'default': {
+     #   'ENGINE': "django.db.backends.postgresql_psycopg2",
+      #  'HOST': os.environ['HOST'],
+      #  'NAME': os.environ['NAME'],
+      #  'USER': os.environ['USER'],
+       # 'PASSWORD': os.environ['PASSWORD'],
+        #'PORT': os.environ['PORT'],
+
+    #}
+#}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
