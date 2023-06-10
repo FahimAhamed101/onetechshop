0d8e8647211d4dc73b4644f0642c8d6990af4cf8
fahim <fahimahamedweb@gmail.com>
2023-06-11 02:15:28 +0600

    agjja'

--- webshop/settings.py
+++ webshop/settings.py
@@ -164,13 +164,18 @@ EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool)
 #}
 
 DATABASES = {
-    {
-   'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.environ.get("DB_NAME"),
-        'USER': os.environ.get("DB_USER"),
-        'PASSWORD': os.environ.get("DB_PASSWORD"),
-        'HOST': os.environ.get("DB_HOST"),
-        'PORT': os.environ.get("DB_PORT"),
+   'default': {
+        'ENGINE': "django.db.backends.postgresql_psycopg2",
+        
+        
+        
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
+       
+
     }
 }
 
