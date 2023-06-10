4a9d3c80891be34a2363305586e1d1b37bc7e4be
fahim <fahimahamedweb@gmail.com>
2023-06-11 00:45:55 +0600

    added

--- webshop/settings.py
+++ webshop/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+from decouple import config
 from pathlib import Path
 import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,8 +25,8 @@ SECRET_KEY = 'django-insecure-!ha15@q#p11dbwkd$(rev&hutx2ohqrwfr0rc_d^sox@ub)ee7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
-
+#ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -37,8 +37,23 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'category',
+    'accounts',
+    'store',
+    'carts',
+       'orders',
+       'django.contrib.sites',
+        'allauth',
+        'allauth.account',
+    'allauth.socialaccount',
+    'allauth.socialaccount.providers.google',
+'allauth.socialaccount.providers.facebook',
 ]
-
+SITE_ID = 5
+ACCOUNT_EMAIL_REQUIRED=True ,
+ACCOUNT_USERNAME_REQURIED=True
+LOGIN_REDIRECT_URL = "/"
+ 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -48,6 +63,60 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+
+SOCIALACCOUNT_LOGIN_ON_GET =True
+ACCOUNT_AUTHENTICATED_LOGIN_REDIRECTS =True
+SOCIAL_AUTH_REDIRECT_IS_HTTPS = True
+ACCOUNT_DEFAULT_HTTP_PROTOCOL = "https"
+
+AUTHENTICATION_BACKENDS = [
+    
+    # Needed to login by username in Django admin, regardless of `allauth`
+    'django.contrib.auth.backends.ModelBackend',
+
+    # `allauth` specific authentication methods, such as login by e-mail
+    'allauth.account.auth_backends.AuthenticationBackend',
+
+]
+SOCIALACCOUNT_PROVIDERS = {
+   
+    "google": {
+        # For each OAuth based provider, either add a ``SocialApp``
+        # (``socialaccount`` app) containing the required client
+        # credentials, or list them here:
+       
+     
+        # These are provider-specific settings that can only be
+        # listed here:
+        "SCOPE": [
+            "profile",
+            "email",
+        ],
+        "AUTH_PARAMS": {
+            "access_type": "online",
+        }
+    },
+    'facebook':
+{'METHOD': 'oauth2',
+'SCOPE': ['email','public_profile', 'user_friends'],
+'AUTH_PARAMS': {'auth_type': 'reauthenticate'},
+'FIELDS': [
+'id',
+'email',
+'name',
+'first_name',
+'last_name',
+'verified',
+'locale',
+'timezone',
+'link',
+'gender',
+'updated_time'],
+'EXCHANGE_TOKEN': True,
+'LOCALE_FUNC': lambda request: 'kr_KR',
+'VERIFIED_EMAIL': False,
+'VERSION': 'v2.4'}}
 
 ROOT_URLCONF = 'webshop.urls'
 
@@ -62,22 +131,37 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                 'category.context_processors.menu_links',
+                'carts.context_processors.counter',
             ],
         },
     },
 ]
 
 WSGI_APPLICATION = 'webshop.wsgi.application'
-
-
+AUTH_USER_MODEL= 'accounts.Account'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR /'media'
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+}
+CROS_ORIGIN_ALLOW_ALL = True
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# SMTP configuration
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_PORT = config('EMAIL_PORT', cast=int)
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool)
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-#DATABASES = {
- #    'default': {
- #       'ENGINE': 'django.db.backends.sqlite3',
- #        'NAME': BASE_DIR / 'db.sqlite3',
- #    }
-#}
+DATABASES = {
+     'default': {
+       'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 # DATABASES = {
     #'default': {
      #   'ENGINE': "django.db.backends.postgresql_psycopg2",
@@ -120,16 +204,15 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-#STATICFILES_DIRS = [
- #   BASE_DIR / "static",]
+STATICFILES_DIRS = [
+    BASE_DIR / "static",]
     
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+#STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 #STATIC_ROOT = "/static/"
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
