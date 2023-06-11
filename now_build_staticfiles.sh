dfe3a88601c01e689a5db5cc79288ad70e258ff6
fahim <fahimahamedweb@gmail.com>
2023-06-11 16:30:20 +0600

    akhdkhad

--- now_build_staticfiles.sh
+++ now_build_staticfiles.sh
@@ -1,3 +1,8 @@
 # now_build_staticfiles.sh
 
 # Install Python 3.6 since it is missing in the Now build environment
+pip install -r requirements.txt
+
+# make migrations
+python3.9 manage.py migrate 
+python3.9 manage.py collectstatic
