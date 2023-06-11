f573082ce95d1b4cc10b4205adfac4c53816ee0a
fahim <fahimahamedweb@gmail.com>
2023-06-11 16:28:50 +0600

    akhdkahd

--- now_build_staticfiles.sh
+++ now_build_staticfiles.sh
@@ -1,13 +1,3 @@
 # now_build_staticfiles.sh
 
 # Install Python 3.6 since it is missing in the Now build environment
-
-# Install project requirements
-pip install -r requirements.txt
-
-# Build staticfiles
-python manage.py collectstatic
-python manage.py makemigrations
-python manage.py migrate --run-syncdb
-python manage.py makemigrations sessions
-python manage.py migrate sessions
